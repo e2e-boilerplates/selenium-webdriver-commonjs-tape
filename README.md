a0b20f6a79c930154424b9261a3ff448dfb27bb1
Girma Nigusse <xgirma@users.noreply.github.com>
2020-02-01 17:38:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# selenium-webdriver: (CommonJS, Jasmine and Expect)
+
+[![GitHub Actions status | e2e-boilerplates/selenium-webdriver-commonjs-jasmine-expect](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-jasmine-expect/workflows/selenium-webdriver-commonjs-jasmine-expect/badge.svg)](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-jasmine-expect/actions?workflow=selenium-webdriver-commonjs-jasmine-expect)
+
+selenium-webdriver end-to-end test automation boilerplate, using CommonJS, Jasmine and Expect.
+
+## Getting Started
+
+    git clone git@github.com:e2e-boilerplates/selenium-webdriver-commonjs-jasmine-expect.git
+    cd selenium-webdriver-commonjs-jasmine-expect
+    npm install
+    npm run test
---
+++ spec/sandbox.spec.js
@@ -0,0 +1,31 @@
+const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+
+require("chromedriver");
+
+const options = new chrome.Options();
+const chromeOptions = process.env.GITHUB_ACTIONS ? options.headless() : options;
+
+describe("Sandbox", () => {
+  let browser;
+
+  beforeAll(async () => {
+    browser = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(chromeOptions)
+      .build();
+    browser.get("https://e2e-boilerplates.github.io/sandbox/");
+  }, 20000);
+
+  afterAll(() => {
+    browser.quit();
+  });
+
+  it("should be on Sandbox", async () => {
+    const title = await browser.getTitle();
+    const header = await browser.findElement(By.css("h1"));
+
+    expect(title).toEqual("Sandbox");
+    expect(await header.getText()).toEqual("Sandbox");
+  });
+});
